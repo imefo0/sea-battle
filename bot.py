6b01bf2b9478b01f2f1ed977018fab78c47fa9d8
imefo <example@example.com>
2026-05-08 18:03:54 +0500

    Добавлена механика рандомного бота

--- bot.py
+++ bot.py
@@ -1,11 +1,17 @@
-def greenhorn(): # юнга, рандом
-    pass
+import random
+
+def greenhorn(field): # юнга, рандом
+    return [random.randint(0, 9), random.randint(0, 9)]
+
 def harpooner(): # гарпунер, охотник
     pass
+
 def navigator(): # штурман, шахматный
     pass
+
 def admiral(): # адмирал, тепловая карта
     pass
+
 def master_seawolf(): # мастер Морской волк, карта + история
     pass
 
