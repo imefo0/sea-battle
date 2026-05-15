0d271c6dc43428e94bb7b7a9a3793db106a9a831
imefo <example@example.com>
2026-05-15 16:26:54 +0500

    bugs fixing in main

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
+# импорт всех нужных библиотек
 import time
 import os
 import random
-import bot
 
-true = True
-false = False
+# ВЕЗДЕ [x, y], НИКАКОГО xy И ДРУГОЙ ЧУШИ!!!!!!!
 
+# поле для игрока
 player_field = []
+player_ships = []
+
+# поле где игрок атакует
 player_radar = []
+
+# поле бота
 bot_field = []
+bot_ships = []
+
+# поле где бот атакует
 bot_radar = []
 
+# само поле из скрытых пустышек
 field = [
     [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
     [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
@@ -23,143 +32,160 @@ field = [
     [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
     [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
 ]
-emoji = ["🌊", "🚢", "🌊", "🔹", "💥", "💀"]
-""" 0 - скрытый корабль 🌊, 
-1 - открытый корабль 🚢, 
-2 - скрытая пустышка 🌊, 
-3 - открытая пустышка 🔸, 
-4 - попал 💥
-5 - убит 💀
 
-когда атака в:
-0 -> 4
-2 -> 3
-1 - после игры/при расстановке кораблей
-5 - когда весь корабль убит
-"""
+# перечисление эмоджи
+emoji = ["🌊", "🚢", "🌊", "🔹", "💥", "💀"]
+# 0 - скрытый корабль 🌊, 
+# 1 - открытый корабль 🚢, 
+# 2 - скрытая пустышка 🌊, 
+# 3 - открытая пустышка 🔸, 
+# 4 - попал 💥
+# 5 - убит 💀
 
 def clear():
     os.system("clear")
 
+# перевод в эмоджи
 def translate_to_emoji(num):
     return emoji[num]
 
+# перевод в y через буквы
 def translate_from_word(word):
     words = ["А", "Б", "В", "Г", "Д", "Е", "Ж", "З", "И", "К"]
     return words.index(word.capitalize())
 
+# перевод из y в буквы
 def translate_to_num(list):
     return f"{''.join(map(str, list))}"
 
 def create_field():
     return [[2 for _ in range(10)] for _ in range(10)]
 
+
+# печатаем поле
 def print_field(field):
     words = ["А", "Б", "В", "Г", "Д", "Е", "Ж", "З", "И", "К"]
 
+    # пишем все вертикали в виде букв
     print(end="   ")
     for i in words:
         print(i, end="  ")
     print()
 
+    # перебор в списке поля
     for i in range(len(field)):
+        # пишем пробел, горизонталь, если это i=9 то
+        # не пишем перед ним пробел
         print(f" {i+1}" if i != 9 else f"{i+1}", end=" ")
+        # пишем каждую клетку
         for j in field[i]:
             print(translate_to_emoji(j), end=" ")
         print()
 
-def change_cell(cell_plus_num):
-    field[int(cell_plus_num[1])][
-        int(cell_plus_num[0])] = int(cell_plus_num[2])
+# сменить ячейку
+def change_cell(cell, num, field):
+    # выбираем field[y][x] и меняем на c_p_n[2]
+    field[cell[1]][cell[0]] = num
 
+# расставить случайно клетки
 def randomize():
     for i in range(len(field)):
-        for j in field[i]:
-            field[random.randint(0, 9)][
-                random.randint(0, 9)] = random.choice([0, 2])
+        for j in range(len(field[i])):
+            # берем кажду клетку и ставим либо 0 либо 2
+            field[i][j] = random.choice([0, 2])
 
 def fire(cell):
-    x = int(cell[0])
-    y = int(cell[1])
+    x, y = cell
+    # если в клетку, которую мы стреляли является 
+    # скрытым корабликом то меняем на огонь
     if field[y][x] == 0:
         field[y][x] = 4
+    
+    # если скрытая пустышка то меняем на открытую
     elif field[y][x] == 2:
         field[y][x] = 3
 
-def set_ship_OLD(a, b):
-    ya = translate_from_word(a[0])
-    yb = translate_from_word(b[0])
-    xa = int(a[1]) - 1
-    xb = int(b[1]) - 1
-    maxx = max(xa, xb)
-    minx = min(xa, xb)
-    maxy = max(ya, yb)
-    miny = min(ya, yb)
-    if xa == xb and maxy - miny <= 3:
-        for i in range(miny, maxy+1):
-            change_cell(f"{i}{xa}1")
-    elif ya == yb and maxx - minx <= 3:
-        for i in range(minx, maxx+1):
-            change_cell(f"{i}{ya}1")
     else:
-        print("NO")
-        return
+        return False
     
-def set_ship_1():
-    pass
+    return True
 
-def set_ship_2(coordinate, direction, num: int, is_player: bool):
-    x = int(coordinate[0])
-    y = int(coordinate[1])
-    d = direction
-    
-    try:
-        match d:
-            case ">":
-                if x + num > 10: raise IndexError() 
-            case "<":
-                if x - num < 1: raise IndexError() 
-            case "^":
-                if y - num < 10: raise IndexError()
-            case "v":
-                if y + num > 1: raise IndexError()
-            case _: raise IndexError() 
+def set_ship1(cell1, cell2, field):
+    dx = abs(cell1[0] - cell2[0])
+    dy = abs(cell1[1] - cell2[1])
 
-        for n in range(num): #исправить, убрать цикл, делать как x+n 
-            change_cell(f"{x}{y}{1}")
-            if d == ">": x += 1
-            elif d == "<": x -= 1
-            elif d == "^": y -= 1
-            elif d == "v": y += 1
-            else: raise IndexError() 
+    if dx != 0 and dy != 0: return False
+
+    axis = 1 if dx == 0 else 0
+    steps = dy if dx == 0 else dx
+
+    cell = list(cell1 if cell1[axis] < cell2[axis] else cell2)
+
+    for _ in range(steps + 1):
+        change_cell(cell, 0, field)
+        cell[axis] += 1
     
+    return True
+
+# на входе получаем координату только носа корабля,
+# направление построения, сколько палуб и само поле
+# куда будем его пихать
+# coordinate -> cell
+# direction -> dir
+def set_ship2(cell, dir, num, field):    
+    # dir принимает udlr и ^v<>
+    # для определения смещения dir
+    delta = {
+        "v": (0, 1), "d": (0, 1),
+        "^": (0, -1), "u": (0, -1),
+        "<": (-1, 0), "l": (-1, 0),
+        ">": (1, 0), "r": (1, 0)
+    }
+    # узнаем смещение
+    offset = delta.get(dir)
+
+    # если неверный ввод
+    if offset == None: return False
+
+    # находим начальные координаты и 
+    # изменение для следующей палубы
+    dx, dy = offset
+    x, y = list(cell)
+
+    # предпологаем в каких координатах 
+    # будет конец корабля
+    final_x = x + dx * num
+    final_y = y + dy * num
+
+    # если корабль выходит за пределы карты
+    if any([final_x < -1, final_y < -1,
+            final_x > 10, final_y > 10]):
+        return False
+
+    # заменяем каждую клетку которую надо
+    # на корабль
+    for _ in range(num):
+        change_cell([x, y], 0, field)
+        x += dx
+        y += dy
+
+    return True
     # добавить механику добавления корабля в список 
     # ships_player/bot_has
-        
-    except IndexError:
-        print("It is impossible to place the ship in this way")
-
-def bot_move(bot_name):
-    if bot_name in ["greenhorn", "random", "1"]:
-        fire(translate_to_num(bot.greenhorn(field)))
-
-    elif bot_name in ["harpooner", "hunter", "2"]:
-        pass
-
-    elif bot_name in ["navigator", "chessman", "3"]:
-        pass
-    
-    elif bot_name in ["admiral", "heat", "4"]:
-        pass
-    
-    elif bot_name in ["master seawolf", "heat+history", "5"]:
-        pass
-    
-    else:
-        pass
-
-def player_move():
-    pass
 
 # нормально сделать чтобы корды были везде через [x, y], 
 # а не как попало
+while True:
+    print_field(field)
+
+    #raw_cell, dir, num = input().split(" ")
+    #cell = [int(i) for i in raw_cell.split(",")]
+    #print(set_ship2(cell, dir, int(num), field))
+
+    raw_cell1, raw_cell2 = input().split(" ")
+    cell1 = [int(i) for i in raw_cell1.split(",")]
+    cell2 = [int(i) for i in raw_cell2.split(",")]
+    print(set_ship1(cell1, cell2, field))
+
+    time.sleep(0.5)
+    clear()
