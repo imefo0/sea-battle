6ce36b50687576ce26f5b61ecf15a02b42861b50
imefo <example@example.com>
2026-05-19 21:04:28 +0500

    added automatic outlining of misses of killed ship

--- bot.py
+++ bot.py
@@ -33,6 +33,9 @@ def clear_ship(field, ships, ship_idx):
             if isinstance(part, list):
                 x, y, _ = part
                 field[y][x] = 5
+                for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    if field[y + dy][x + dx] == 2: 
+                        field[y + dy][x + dx] = 3
 
     else: return False
     return True
